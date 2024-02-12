6005c63b34a6326777ddf9ffd626179a7ac50eb3
Ankur Thakre <71597352+Thakre02@users.noreply.github.com>
2024-02-12 09:02:02 +0000

    conversion data typr

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = "grow"
+
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true=>1; false=>0
+
+let loggedIn = ""
+
+let booleanIs = Boolean(loggedIn)
+console.log(booleanIs);
+
+// 1=> true; 0=> false
+// "" => false
+// "" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
