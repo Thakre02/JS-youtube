f1aaeb0996d90b41c9f9ff5ac7dc48329255b431
Ankur Thakre <71597352+Thakre02@users.noreply.github.com>
2024-02-14 09:12:49 +0000

    operrations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -26,3 +26,42 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log( typeof stringNumber);
+
+// **************************** OPERATIONS *******************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2); Addition
+// console.log(2-2); Subtraction
+// console.log(2*2); Multiplication
+// console.log(2/2); Division
+// console.log(2%2); Modulos
+// console.log(2**2); Power
+
+let str1 = "hello "
+let str2 = " namaste"
+str3 = str1+str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(2 + "2");
+console.log("1" + 2 + 2);
+console.log(2 + 2 + "1");
+
+console.log( ( 3 + 4 ) * 5 % 3 );
+
+console.log(+true);
+
+
+let num1,num2,num3;
+
+num1=num2=num3=2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// js and mdmn
